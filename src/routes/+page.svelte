a7f60bdc6289b135dac7de28ca19e63dc3de2640
Kaleb-cpu <kakuj424@gmail.com>
2023-03-14 17:54:21 -0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Hey Tony!</h1>
+<p>Visit <a href="https://sait-wbdv-w23.netlify.app/courses/cpnt-262/day-16">March 15 materials</a> to know what you will be teaching us tomorrow</p>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,2 @@
+<h1>About</h1>
+<p>Welcome! Get to know me Here</p>
