f8ce1ff26655a1267abcab70d1cb89729335e9ca
Kaleb-cpu <kakuj424@gmail.com>
2023-03-20 19:34:44 -0600

    achievement 18

--- src/routes/achievements/+page.svelte
+++ src/routes/achievements/+page.svelte
@@ -2,5 +2,6 @@
 
 <ul>
   <li><h2><a href="./achievements/achievement 16">Achievement 16 - Todos-List</a></h2></li>
-  <li><h2><a href="./achievements/achievement 17">Achievement 17 - Push of a Button</a></h2></li>   
+  <li><h2><a href="./achievements/achievement 17">Achievement 17 - Push of a Button</a></h2></li>
+  <li><h2><a href="./achievements/achievement 18">Achievement 18 - Todos-List</a></h2></li>   
 </ul>
