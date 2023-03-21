80d3e6bb20704520a04bbe1539f8e92788df0304
Kaleb-cpu <kakuj424@gmail.com>
2023-03-21 11:35:44 -0600

    achievement 19 ol added

--- src/routes/achievements/+page.svelte
+++ src/routes/achievements/+page.svelte
@@ -4,4 +4,5 @@
   <li><h2><a href="./achievements/achievement 16">Achievement 16 - Todos-List</a></h2></li>
   <li><h2><a href="./achievements/achievement 17">Achievement 17 - Push of a Button</a></h2></li>
   <li><h2><a href="./achievements/achievement 18">Achievement 18 - Todos-List</a></h2></li>   
+  <li><h2><a href="./achievements/achievement 19">Achievement 19 - Exported Todos</a></h2></li> 
 </ul>
---
+++ src/routes/achievements/achievement 19/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+import { todo } from './todos.js'
+</script>
+ 
+
+<h1>Achievement 19</h1>
+
+<p>These are Kalebs Morning to do list</p>
+<ol>
+    {#each todo as todo}
+    <li>{todo}</li>
+    {/each}
+  </ol>
