8c2d42875b32ae67b1779a808eb70125d2d217e2
Kaleb-cpu <kakuj424@gmail.com>
2023-03-22 15:38:18 -0600

    first letter capital script added

--- src/routes/achievements/achievement-20/[name]/+page.svelte
+++ src/routes/achievements/achievement-20/[name]/+page.svelte
@@ -1,14 +1,19 @@
 <script>
   import { page } from '$app/stores'
   console.log($page.params.name);
+
+   const capitalizeFirstSentence = ($page.params.name)[0].toUpperCase() + ($page.params.name).slice(1)
+
+   export const nameCapital = capitalizeFirstSentence
 </script>
 
-  <h1>Your name is {$page.params.name} </h1>
+  <h1>Your name is {capitalizeFirstSentence} </h1>
 
    
    <style>
     h1{
       text-align: center;
       margin-top: 10vh;
+      
     }
    </style>
--- src/routes/achievements/achievement-20/[name]/[age]/+page.svelte
+++ src/routes/achievements/achievement-20/[name]/[age]/+page.svelte
@@ -1,12 +1,12 @@
 <script>
   import { page } from '$app/stores'
   console.log($page.params.age);
+
+  const capitalizeFirstSentence = ($page.params.name)[0].toUpperCase() + ($page.params.name).slice(1)
+
 </script>
 
-
-
-<h1>{$page.params.name}'s age is {$page.params.age}</h1>
-<p></p>
+<h1>{capitalizeFirstSentence}'s age is {$page.params.age}</h1>
 
 <style>
     h1{
