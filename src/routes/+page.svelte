8f89d22d9cf79d431c6400485242ad68baf36280
Kaleb <122635821+Kaleb-cpu@users.noreply.github.com>
2023-05-07 11:04:23 -0600

    h1 updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 
-<main><h1>Hey Tony!</h1></main>
+<main><h1>Hello!</h1></main>
 
 <p>Visit <a href="https://sait-wbdv-w23.netlify.app/courses/cpnt-262/day-16">March 15 materials</a> to know what you will be teaching us tomorrow</p>
 
